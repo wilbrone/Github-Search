f82871114799d68552c6e44e35c8842fbcdb069c
wilbrone <wilbroneokoth@gmail.com>
2019-11-04 10:44:53 +0300

    Auto-generated commit

--- src/app/constants/app-const.ts
+++
@@ -1,4 +0,0 @@
-export class AppConst {
-  public static serverPath = "https://api.github.com/users/";
-  public static Path = "https://api.github.com/search/repositories?q=";
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  APIKEY: "8508cae15aa06b939ecf129e0ce110c47c734986"
-};
