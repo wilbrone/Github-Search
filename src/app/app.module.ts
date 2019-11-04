a3937a7e54618b026baba51cefe7d90e5e18429e
wilbrone <wilbroneokoth@gmail.com>
2019-11-04 04:51:03 +0300

    add styling to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatRippleModule} from '@angular/material/core';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import { GitPageComponent } from './components/git-page/git-page.component';
 import { SearchDetailComponent } from './components/search-detail/search-detail.component';
@@ -38,7 +39,8 @@ import { SearchDetailComponent } from './components/search-detail/search-detail.
     MatToolbarModule,
     MatCardModule,
     MatIconModule,
-    MatRippleModule
+    MatRippleModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
